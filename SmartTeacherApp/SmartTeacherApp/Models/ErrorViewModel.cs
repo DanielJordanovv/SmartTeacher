fabaa0bda38a25155ceb91837705f4f43ab53316
Daniel Jordanov <d.jordanov.dev@gmail.com>
2023-12-05 12:18:26 +0200

    Test commit

---
+++ SmartTeacherApp/SmartTeacherApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartTeacherApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
