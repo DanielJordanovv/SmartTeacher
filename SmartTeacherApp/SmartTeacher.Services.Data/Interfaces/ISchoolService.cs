8179969c42053a96220e1017f72270c14402c72d
Християн Христов <itco.official18@gmail.com>
2024-02-02 15:42:08 +0200

    School Service

--- SmartTeacherApp/SmartTeacher.Services.Data/Interfaces/ISchoolService.cs
+++ SmartTeacherApp/SmartTeacher.Services.Data/Interfaces/ISchoolService.cs
@@ -1,6 +1,17 @@
-﻿namespace SmartTeacher.Services.Data.Interfaces
+﻿using SmartTeacher.Data.Models;
+using SmartTeacher.Web.ViewModels.School;
+
+namespace SmartTeacher.Services.Data.Interfaces
 {
     public interface ISchoolService
     {
+        Task<IEnumerable<AllSchoolsViewModel>> AllAsync();
+        Task<IEnumerable<AllSchoolsViewModel>> AllSearchedAsync(string search);
+        Task<SchoolDetailsViewModel> GetDetailsByIdAsync(string id);
+        Task CreateAsync(AddSchoolViewModel model);
+        Task UpdateAsync(string id, EditSchoolViewModel model);
+        Task DeleteAsync(string id);
+        Task<School> GetSchoolAsync(string id);
+        bool SchoolExists(string id);
     }
 }
