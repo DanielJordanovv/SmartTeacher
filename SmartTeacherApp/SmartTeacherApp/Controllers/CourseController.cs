bbd2dcce18e90cc7724d1f8200c1f9d9f46322e4
Християн Христов <itco.official18@gmail.com>
2024-02-02 15:58:43 +0200

    small changes

--- SmartTeacherApp/SmartTeacherApp/Controllers/CourseController.cs
+++ SmartTeacherApp/SmartTeacherApp/Controllers/CourseController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartTeacher.Services.Data.Interfaces;
 
 namespace SmartTeacherApp.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly ICourseService service;
+        
+        public CourseController(ICourseService service)
+        {
+            this.service = service;
+        }
+
         public IActionResult Index()
         {
             return View();
--- SmartTeacherApp/SmartTeacherApp/Controllers/SchoolController.cs
+++ SmartTeacherApp/SmartTeacherApp/Controllers/SchoolController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartTeacher.Services.Data.Interfaces;
 
 namespace SmartTeacherApp.Controllers
 {
     public class SchoolController : Controller
     {
+        private readonly ISchoolService service;
+
+        public SchoolController(ISchoolService service)
+        {
+            this.service = service;
+        }
         public IActionResult Index()
         {
             return View();
